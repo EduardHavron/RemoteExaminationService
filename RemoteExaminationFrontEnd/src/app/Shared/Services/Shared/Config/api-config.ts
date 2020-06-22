81c524e7c6c17b6381b4e70c562c6a8d55397fa3
stakhevychK <48862945+stakhevychK@users.noreply.github.com>
2020-06-22 19:47:35 +0300

    ApiPath fix

--- RemoteExaminationFrontEnd/src/app/Shared/Services/Shared/Config/api-config.ts
+++ RemoteExaminationFrontEnd/src/app/Shared/Services/Shared/Config/api-config.ts
@@ -1,4 +1,4 @@
 export enum ApiConfig {
   apiProductionPath = 'https://localhost:5001/api/',
-  apiPath = 'https://reservice.azurewebsites.net/api'
+  apiPath = 'https://reservice.azurewebsites.net/api/'
 }
