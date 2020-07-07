81a6907a649ed2ff4b4e235649593d56311b1bab
stakhevychK <48862945+stakhevychK@users.noreply.github.com>
2020-07-07 22:10:10 +0300

    Create exam component styling

--- RemoteExaminationFrontEnd/src/app/Exam/Pages/exam-create/exam-create.component.ts
+++ RemoteExaminationFrontEnd/src/app/Exam/Pages/exam-create/exam-create.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {NbToastrService} from '@nebular/theme';
+import {NbToastrService, NbWindowService} from '@nebular/theme';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ExamService} from '../../../Shared/Services/Exam/exam.service';
-import {faCheck, faPlus} from '@fortawesome/free-solid-svg-icons';
+import {faCheck, faPlus, faSave } from '@fortawesome/free-solid-svg-icons';
 import {IAnswer} from '../../../Shared/Models/ExamView/Interfaces/Answer/IAnswer';
 import {IExam} from '../../../Shared/Models/ExamView/Interfaces/Exam/IExam';
 import {IQuestion} from '../../../Shared/Models/ExamView/Interfaces/Question/IQuestion';
@@ -15,19 +15,21 @@ import {IQuestion} from '../../../Shared/Models/ExamView/Interfaces/Question/IQu
 })
 
 export class ExamCreateComponent implements OnInit {
-  faPlus = faPlus;
-  faCheck = faCheck;
-
-  examNameForm: FormGroup;
-  exam: IExam<IQuestion<IAnswer>>;
 
   constructor(private examService: ExamService,
               private fb: FormBuilder,
-              private toastrService: NbToastrService) {
+              private toastrService: NbToastrService,
+              private popupWindowService: NbWindowService) {
     this.examNameForm = this.fb.group({
       examName: ['', Validators.required]
     });
   }
+  faPlus = faPlus;
+  faCheck = faCheck;
+  faSave = faSave;
+
+  examNameForm: FormGroup;
+  exam: IExam<IQuestion<IAnswer>>;
 
   showToast(position, status, duration, message: string, title: string) {
     this.toastrService.show(
@@ -40,5 +42,21 @@ export class ExamCreateComponent implements OnInit {
     this.examService.getExamById(1).subscribe((data) => {
       this.exam = data;
     });
+  }
+
+  addAnswer(event) {
+    event.target.hide();
+  }
+
+  addQuestion() {
+  }
+
+  createExam() {
+  }
+
+  saveAnswer() {
+  }
+
+  saveQuestion() {
   }
 }
