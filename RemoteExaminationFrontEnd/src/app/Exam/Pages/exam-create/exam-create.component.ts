0f4ae89f90b419b6b0a45ed053c88d65839d6a46
stakhevychK <48862945+stakhevychK@users.noreply.github.com>
2020-07-09 22:28:41 +0300

    Create Exam Page is under development

--- RemoteExaminationFrontEnd/src/app/Exam/Pages/exam-create/exam-create.component.ts
+++ RemoteExaminationFrontEnd/src/app/Exam/Pages/exam-create/exam-create.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {NbToastrService, NbWindowService} from '@nebular/theme';
+import {NbToastrService} from '@nebular/theme';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ExamService} from '../../../Shared/Services/Exam/exam.service';
-import {faCheck, faPlus, faSave } from '@fortawesome/free-solid-svg-icons';
+import {faCheck, faPlus, faSave, faTrash } from '@fortawesome/free-solid-svg-icons';
 import {IAnswer} from '../../../Shared/Models/ExamView/Interfaces/Answer/IAnswer';
 import {IExam} from '../../../Shared/Models/ExamView/Interfaces/Exam/IExam';
 import {IQuestion} from '../../../Shared/Models/ExamView/Interfaces/Question/IQuestion';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -16,20 +17,24 @@ import {IQuestion} from '../../../Shared/Models/ExamView/Interfaces/Question/IQu
 
 export class ExamCreateComponent implements OnInit {
 
+  faPlus = faPlus;
+  faCheck = faCheck;
+  faSave = faSave;
+  faTrash = faTrash;
+  hiddenBlock;
+  showedBlock;
+  parentBlock;
+  examNameForm: FormGroup;
+  exam: IExam<IQuestion<IAnswer>>;
+
   constructor(private examService: ExamService,
               private fb: FormBuilder,
               private toastrService: NbToastrService,
-              private popupWindowService: NbWindowService) {
+              private router: Router) {
     this.examNameForm = this.fb.group({
       examName: ['', Validators.required]
     });
   }
-  faPlus = faPlus;
-  faCheck = faCheck;
-  faSave = faSave;
-
-  examNameForm: FormGroup;
-  exam: IExam<IQuestion<IAnswer>>;
 
   showToast(position, status, duration, message: string, title: string) {
     this.toastrService.show(
@@ -45,18 +50,78 @@ export class ExamCreateComponent implements OnInit {
   }
 
   addAnswer(event) {
-    event.target.hide();
+    this.showAnswerInput(event);
   }
 
   addQuestion() {
+    this.showQuestionInput();
   }
 
+  removeAnswer(questionIndex: number, answerIndex: number) {
+    this.exam.questions[0].answers
+      .splice(answerIndex, 1);
+  }
+  removeQuestion(questionIndex: number) {
+    this.exam.questions
+      .splice(questionIndex, 1);
+  }
   createExam() {
+    this.examService.createExam(this.exam).subscribe(() => {
+      this.showToast('top-right',
+        'success',
+        3000,
+        'Экзамен успешно создан',
+        'Успех');
+    });
   }
 
   saveAnswer() {
+    this.showAnswerInitial();
   }
 
   saveQuestion() {
+    const question = $('#question-input')
+      .val()
+      .toString();
+    this.exam.questions.push({questionMessage: question, answers: []});
+    this.showQuestionInitial();
+  }
+
+  showAnswerInput(event) {
+    this.parentBlock = $(event.path[0])
+      .parents('nb-list-item')
+      .first();
+    this.hiddenBlock = this.parentBlock
+      .find('.show-answer')
+      .first();
+    this.showedBlock = this.parentBlock
+      .find('.save-answer')
+      .first();
+
+    this.hiddenBlock
+      .addClass('hide');
+    this.showedBlock
+      .removeClass('hide');
+  }
+
+  showAnswerInitial() {
+    this.hiddenBlock
+      .removeClass('hide');
+    this.showedBlock
+      .addClass('hide');
+
+    this.showedBlock = undefined;
+    this.hiddenBlock = undefined;
+    this.parentBlock = undefined;
+  }
+
+  showQuestionInput() {
+    $('#add-question-block').addClass('hide');
+    $('#save-question-block').removeClass('hide');
+  }
+
+  showQuestionInitial() {
+    $('#add-question-block').removeClass('hide');
+    $('#save-question-block').addClass('hide');
   }
 }
