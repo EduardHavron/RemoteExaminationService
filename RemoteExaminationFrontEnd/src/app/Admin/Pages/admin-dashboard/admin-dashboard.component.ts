5c50baeb9fd207b6ad614c2080948fafcb43c8f4
stakhevychK <48862945+stakhevychK@users.noreply.github.com>
2020-08-23 16:41:18 +0300

    Stupid fixes needed only for course work

--- RemoteExaminationFrontEnd/src/app/Admin/Pages/admin-dashboard/admin-dashboard.component.ts
+++ RemoteExaminationFrontEnd/src/app/Admin/Pages/admin-dashboard/admin-dashboard.component.ts
@@ -6,11 +6,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-
+isPhone: boolean;
   constructor() {
   }
 
   ngOnInit() {
+    this.checkIfPhone();
   }
 
+  checkIfPhone() {
+    if (window.innerWidth < window.innerHeight) {
+      this.isPhone = true;
+    }
+  }
 }
